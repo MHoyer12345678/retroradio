0b620a8889c4f18a3293d75b133526eec3efff99
MHoyer12345678 <mhoyer.oss-devel@freenet.de>
2022-03-06 23:21:47 +0100

    cleaning up project

--- source-project-configs.sh
+++ source-project-configs.sh
@@ -1,2 +1,2 @@
 # crossbuild Kernel scripts config
-export KNL_SCRIPTS_CONF_FILE=/home/joe/work/retroradio-zwei/kernel/cross-kernel-tools.conf
+export KNL_SCRIPTS_CONF_FILE=/home/joe/work/retroradio/kernel/cross-kernel-tools.conf
